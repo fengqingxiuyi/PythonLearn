b1a5131c7a229f53474dd26ac2731afe16f1b2d9
fqxyi <fengqingxiuyi@gmail.com>
2022-09-25 10:58:18 +0800

    @feature 浮点类型

---
+++ 06_float.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Date   : 2022/9/25
+@Author : shenBF
+@Desc   : 
+"""
+from decimal import Decimal
+
+print(1.1+2.2)
+
+print(Decimal('1.1')+Decimal('2.2'))
