a832d470d5cc85d181d1b6c568164ae331b107ea
沈彬峰 <shen.binfeng@yalla.live>
2022-09-27 10:41:16 +0800

    @feature add 07_input.py

---
+++ 07_input.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Date   : 2022/9/27
+@Author : shenBF
+@Desc   : 
+"""
+a = input('请输入一个加数：')
+a = int(a)
+b = input('请输入另一个加数：')
+b = int(b)
+print(a + b)
