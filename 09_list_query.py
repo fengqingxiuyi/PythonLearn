4e5fd77c11cbaf1f8e261dba3e95d7762574ce01
沈彬峰 <shen.binfeng@yalla.live>
2022-09-27 16:53:55 +0800

    @feature add 09_list_query.py

---
+++ 09_list_query.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Date   : 2022/9/27
+@Author : shenBF
+@Desc   : 
+"""
+
+lst = ['hello', 'world', 98, 'hello', 'world', 234]
+print(lst[2])
+print(lst[-3])
+print(lst[10])
