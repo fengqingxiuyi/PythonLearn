e92b7131d9344d99ff4064b9ce3571f553d14f16
沈彬峰 <shen.binfeng@yalla.live>
2022-09-27 14:37:50 +0800

    @feature add 08_range.py

---
+++ 08_range.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Date   : 2022/9/27
+@Author : shenBF
+@Desc   : 
+"""
+
+r = range(10)
+print(r)  # range(0, 10)
+print(list(r))  # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+r = range(1, 10)
+print(list(r))  # [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+r = range(1, 10, 2)
+print(list(r))  # [1, 3, 5, 7, 9]
