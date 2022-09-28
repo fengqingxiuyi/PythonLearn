2bafa4b0efce93f18d7c1fa50b8b3adc886fa91c
沈彬峰 <shen.binfeng@yalla.live>
2022-09-28 17:12:21 +0800

    @feature update 11_dict_zip.py

--- 11_dict_zip.py
+++ 11_dict_zip.py
@@ -10,3 +10,10 @@ items = ['Fruits', 'Books', 'Others']
 prices = [96, 78, 85]
 lst = zip(items, prices)
 print(list(lst))  # 输出 [('Fruits', 96), ('Books', 78), ('Others', 85)]
+
+prices2 = [96, 78, 85, 20]
+lst2 = zip(items, prices2)
+print(list(lst2))  # 输出 [('Fruits', 96), ('Books', 78), ('Others', 85)]
+
+lst3 = {item.upper(): price for item, price in zip(items, prices)}
+print(list(lst3))  # 输出 ['FRUITS', 'BOOKS', 'OTHERS']
