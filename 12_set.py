142a431e82294d5c09e692f849fae5b0c0b2762f
沈彬峰 <shen.binfeng@yalla.live>
2022-09-29 16:06:47 +0800

    @feature add 12_set.py

---
+++ 12_set.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Date   : 2022/9/29
+@Author : shenBF
+@Desc   : 
+"""
+
+print({'python', 100})  # 输出 {'python', 100}
+print(set(range(6)))  # 输出 {0, 1, 2, 3, 4, 5}
+print(set([3, 4, 4, 5]))  # 输出 {3, 4, 5}
+print(set((3, 4, 4, 5)))  # 输出 {3, 4, 5}
+print(set('python'))  # 输出 {'h', 'n', 'o', 'p', 'y', 't'}
+print(set({3, 4, 4, 5}))  # 输出 {3, 4, 5}
+print(set())  # 输出 set()
