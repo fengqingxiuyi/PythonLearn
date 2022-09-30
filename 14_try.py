34e5afa9e75224f800100f8cc0c678bc93c8f4ff
沈彬峰 <shen.binfeng@yalla.live>
2022-09-30 17:04:46 +0800

    @feature try...except...else

--- 14_try.py
+++ 14_try.py
@@ -9,10 +9,11 @@ try:
     a = int(input('请输入第一个整数'))
     b = int(input('请输入第二个整数'))
     result = a / b
-    print('结果为：', result)
 except ZeroDivisionError:
     print('除数不允许为0')
 except ValueError:
     print('不能将字符串转换为数字')
 except BaseException as e:
     print(e)
+else:
+    print('结果为：', result)
