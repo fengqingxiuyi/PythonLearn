6bc00c1f8d4d25cbd758aea1f8e93902aaf9af2d
沈彬峰 <shen.binfeng@yalla.live>
2022-09-30 17:01:50 +0800

    @feature add 捕获异常

---
+++ 14_try.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Date   : 2022/9/30
+@Author : shenBF
+@Desc   : 
+"""
+try:
+    a = int(input('请输入第一个整数'))
+    b = int(input('请输入第二个整数'))
+    result = a / b
+    print('结果为：', result)
+except ZeroDivisionError:
+    print('除数不允许为0')
+except ValueError:
+    print('不能将字符串转换为数字')
+except BaseException as e:
+    print(e)
