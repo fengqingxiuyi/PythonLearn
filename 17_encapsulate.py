50ddfdab85995fcf204217a55268b6f3a3cda651
沈彬峰 <shen.binfeng@yalla.live>
2022-10-08 10:06:10 +0800

    @feature 封装

---
+++ 17_encapsulate.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Date   : 2022/10/8
+@Author : shenBF
+@Desc   : 封装
+"""
+
+
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.__age = age  # 年龄不希望在类的外部被使用，所以加了两个__
+
+    def show(self):
+        print(self.name, self.__age)
+
+
+stu = Student('张三', 20)
+stu.show()
+# 在类的外部使用name与age
+print(stu.name)
+# print(stu.__age)
+print(dir(stu))  # 列出stu可以访问的属性与方法
+print(stu._Student__age)
