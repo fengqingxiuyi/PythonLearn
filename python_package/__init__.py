4193e0869f43c51223f383d58577f145a61e8cf2
沈彬峰 <shen.binfeng@yalla.live>
2022-10-08 16:24:26 +0800

    @feature add package

---
+++ python_package/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@Date   : 2022/10/8
+@Author : shenBF
+@Desc   : 
+"""
---
+++ python_package/moduleB.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@Date   : 2022/10/8
+@Author : shenBF
+@Desc   : 
+"""
+
+b = 2
