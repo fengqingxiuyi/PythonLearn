13047388eb15b0377dc2be7c511572dada5c1a0e
沈彬峰 <shen.binfeng@yalla.live>
2022-10-08 14:40:14 +0800

    @feature 模块化

---
+++ module_test/calc.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Date   : 2022/10/8
+@Author : shenBF
+@Desc   : 
+"""
+
+
+def add(a, b):
+    return a + b
+
+
+def sub(a, b):
+    return a - b
